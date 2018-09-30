534c45b49de0c442f71c2232c5dcb9b4d733ee73
Itzhak <mauy1987@gmail.com>
2018-09-30 12:48:08 +0300

    Git init repo via SourceTree

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './Game_comp'
   // ========================================
-  
+  //init Git repo
   ReactDOM.render(<Game />, document.getElementById("root"));
   
 
