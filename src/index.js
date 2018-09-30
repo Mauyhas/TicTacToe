300ca61ede1e0821e54e1e704194944a53b29584
Itzhak <mauy1987@gmail.com>
2018-09-30 12:49:50 +0300

    Brancher

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Game from './Game_comp'
   // ========================================
   //init Git repo
+  //init new branch
   ReactDOM.render(<Game />, document.getElementById("root"));
   
 
